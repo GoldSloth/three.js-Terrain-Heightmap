eedfec67c80dd54f7ad9fccdb1faa6e86e3977fc
Ruth Waterfield <ruth@gareth.lan>
2018-04-14 15:17:23 +0100

    Allignment

--- Utilities/PointerLockUtilities.js
+++ Utilities/PointerLockUtilities.js
@@ -7,27 +7,27 @@ function pointerLockChange(event) {
 }
 
 function addPointerLockListeners() {
-    document.addEventListener( 'pointerlockchange', pointerLockChange, false );
-    document.addEventListener( 'mozpointerlockchange', pointerLockChange, false );
-    document.addEventListener( 'webkitpointerlockchange', pointerLockChange, false );
-    document.getElementById('render').addEventListener( 'click', function ( event ) {
-        // Ask the browser to lock the pointer
-        document.body.requestPointerLock = document.body.requestPointerLock || document.body.mozRequestPointerLock || document.body.webkitRequestPointerLock;
-        if ( /Firefox/i.test( navigator.userAgent ) ) {
-            var fullscreenchange = function ( event ) {
-                if ( document.fullscreenElement === document.body || document.mozFullscreenElement === document.body || document.mozFullScreenElement === document.body ) {
-                    document.removeEventListener( 'fullscreenchange', fullscreenchange );
-                    document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
-                    document.body.requestPointerLock();
-                }
-            }
-                document.addEventListener( 'fullscreenchange', fullscreenchange, false );
-                document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
-                document.body.requestFullscreen = document.body.requestFullscreen || document.body.mozRequestFullscreen || document.body.mozRequestFullScreen || document.body.webkitRequestFullscreen;
-                document.body.requestFullscreen();
-            } else {
-                document.body.requestPointerLock();
-            }
-    }, false );
+  document.addEventListener( 'pointerlockchange', pointerLockChange, false );
+  document.addEventListener( 'mozpointerlockchange', pointerLockChange, false );
+  document.addEventListener( 'webkitpointerlockchange', pointerLockChange, false );
+  document.getElementById('render').addEventListener( 'click', function ( event ) {
+    // Ask the browser to lock the pointer
+    document.body.requestPointerLock = document.body.requestPointerLock || document.body.mozRequestPointerLock || document.body.webkitRequestPointerLock;
+    if ( /Firefox/i.test( navigator.userAgent ) ) {
+      var fullscreenchange = function ( event ) {
+        if ( document.fullscreenElement === document.body || document.mozFullscreenElement === document.body || document.mozFullScreenElement === document.body ) {
+          document.removeEventListener( 'fullscreenchange', fullscreenchange );
+          document.removeEventListener( 'mozfullscreenchange', fullscreenchange );
+          document.body.requestPointerLock();
+        }
+      }
+      document.addEventListener( 'fullscreenchange', fullscreenchange, false );
+      document.addEventListener( 'mozfullscreenchange', fullscreenchange, false );
+      document.body.requestFullscreen = document.body.requestFullscreen || document.body.mozRequestFullscreen || document.body.mozRequestFullScreen || document.body.webkitRequestFullscreen;
+      document.body.requestFullscreen();
+    } else {
+      document.body.requestPointerLock();
+    }
+  }, false );
 
 }
