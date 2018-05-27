68ddb2be264e54e6a3fe280a5a8e0df35f2f172a
GolddolphinSKB-Fexcraftian <games.tudorrose@gmail.com>
2018-05-27 14:12:14 +0100

    Started on implementing perlin noise terrain

--- Utilities/TerrainUtilities/TerrainFromMath.js
+++ Utilities/TerrainUtilities/TerrainFromMath.js
@@ -1,3 +1,36 @@
-function terrainFromMath() {
+function terrainFromMath(heightRange, oSizeX, oSizeY) {
+    var perlin = new SimplexNoise('seed')
+    var perlinMap = []
+    for (var x=0; x<oSizeX; x++) {
+        var temp = []
+        for (var y=0; y<oSizeY; y++) {
+            temp.push(((perlin.noise2D(x,y)+1)/2)*heightRange)
+        }
+        perlinMap.push(temp)
+    }
     
+    if (performanceTest) {
+        var time = performance.now()
+    }
+    var terrain = new Terrain(perlinMap)
+    if (performanceTest) {
+        console.log('Time for creating terrain: ' + (performance.now()-time))
+        var time = performance.now()
+    }
+    terrain.changeRes(sizeX, sizeY)
+    if (performanceTest) {
+        console.log('Time for changing resoloution: ' + (performance.now()-time))
+        var time = performance.now()
+    }
+    terrain.changeMultiplier(magnitudeY)
+    if (performanceTest) {
+        console.log('Time for creating multiplier: ' + (performance.now()-time))
+        var time = performance.now()
+    }
+    terrain.draw()
+    if (performanceTest) {
+        console.log('Time for drawing terrain: ' + (performance.now()-time))
+        var time = performance.now()
+    }
+    main()
 }
